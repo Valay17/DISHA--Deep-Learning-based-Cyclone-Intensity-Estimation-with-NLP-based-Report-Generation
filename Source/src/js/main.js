fcedcdc0ca6bcc08210a4b5a3e40a0bc70662531
Valay <getvalay@gmail.com>
2025-01-14 16:27:05 -0800

    Trial

---
+++ Source/src/js/main.js
@@ -0,0 +1,15 @@
+import './convertUnits.js';
+import './currentWeatherData.js';
+import './dialogAlert.js';
+import './filterForecastData.js';
+import './formHandler.js';
+import './handleError.js';
+import './main.js';
+import './modelUploadData.js';
+import './navigateUser.js';
+import './setLoadingState.js';
+import './themeIcons.js';
+import './toggleTheme.js';
+import './weatherApp.js';
+import './weatherForecastCards.js';
+import './weatherForecastData.js';
--- Source/vite.config.js
+++ Source/vite.config.js
@@ -2,20 +2,28 @@
 //const API_KEY = "2e15438922f629fe6a2605024e986ca2";
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import { createHtmlPlugin } from "vite-plugin-html";
 import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-  base: "/repo-name/", // Replace with your GitHub Pages repo name
+  base: "/DISHA--Deep-Learning-based-Cyclone-Intensity-Estimation-with-NLP-based-Report-Generation/", // Replace with your repository name
   plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: "src/img", // Copies the `img` folder
-          dest: "assets/img",
+    createHtmlPlugin({
+      inject: {
+        data: {
+          injectScript: `<script type="module" src="./src/js/main.js"></script>`,
         },
-      ],
+      },
     }),
-  ],
+      viteStaticCopy({
+        targets: [
+          {
+            src: "./src/img",
+            dest: "./src/",
+          },
+        ],
+      }),
+    ],
   css: {
     postcss: {
       plugins: [autoprefixer],
@@ -23,7 +31,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      input: "./index.html",
+      input: "index.html",
       output: {
         assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
         entryFileNames: "assets/js/[name]-[hash].js",
