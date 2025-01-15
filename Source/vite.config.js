5e5670d2b3de0ee034c64723904aaa30ea84cc6a
Valay <getvalay@gmail.com>
2025-01-14 16:03:36 -0800

    Tester

--- Source/vite.config.js
+++ Source/vite.config.js
@@ -1,17 +1,17 @@
+// Replace 'your-api-key-here' with your actual API key
+//const API_KEY = "2e15438922f629fe6a2605024e986ca2";
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import autoprefixer from "autoprefixer";
 
-// Replace 'your-api-key-here' with your actual API key
-//const API_KEY = "2e15438922f629fe6a2605024e986ca2";
-
 export default defineConfig({
+  base: "/repo-name/", // Replace with your GitHub Pages repo name
   plugins: [
     viteStaticCopy({
       targets: [
         {
-          src: "./src/img",
-          dest: "./src/",
+          src: "src/img", // Copies the `img` folder
+          dest: "assets/img",
         },
       ],
     }),
@@ -21,7 +21,14 @@ export default defineConfig({
       plugins: [autoprefixer],
     },
   },
-  /* define: {
-    API_KEY: JSON.stringify(API_KEY),
-  }, */
+  build: {
+    rollupOptions: {
+      input: "./index.html",
+      output: {
+        assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        chunkFileNames: "assets/js/[name]-[hash].js",
+      },
+    },
+  },
 });
