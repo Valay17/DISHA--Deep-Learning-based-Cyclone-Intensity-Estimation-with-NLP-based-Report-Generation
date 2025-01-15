a63e4cfe44ae40edaf15a15fe9e468b9ab9c9965
Valay <getvalay@gmail.com>
2025-01-14 16:58:36 -0800

    Just Run

--- Source/vite.config.js
+++ Source/vite.config.js
@@ -1,13 +1,15 @@
 // Replace 'your-api-key-here' with your actual API key
 //const API_KEY = "2e15438922f629fe6a2605024e986ca2";
+
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import { createHtmlPlugin } from "vite-plugin-html";
 import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-  base: "/DISHA--Deep-Learning-based-Cyclone-Intensity-Estimation-with-NLP-based-Report-Generation/", // Replace with your repository name
+  base: "//DISHA--Deep-Learning-based-Cyclone-Intensity-Estimation-with-NLP-based-Report-Generation/", // Replace with your repository name
   plugins: [
+    // Inject HTML with custom script
     createHtmlPlugin({
       inject: {
         data: {
@@ -15,23 +17,23 @@ export default defineConfig({
         },
       },
     }),
-      viteStaticCopy({
-        targets: [
-          {
-            src: "./src/img",
-            dest: "./src/",
-          },
-        ],
-      }),
-    ],
+    // Copy static assets to the dist folder
+    viteStaticCopy({
+      targets: [
+        { src: "src/img", dest: "assets" },
+        { src: "src/static", dest: "assets/static" },
+        { src: "src/animated", dest: "assets/animated" },
+      ],
+    }),
+  ],
   css: {
     postcss: {
-      plugins: [autoprefixer],
+      plugins: [autoprefixer()],
     },
   },
   build: {
     rollupOptions: {
-      input: "index.html",
+      input: "./index.html", // Ensure index.html is included
       output: {
         assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
         entryFileNames: "assets/js/[name]-[hash].js",
